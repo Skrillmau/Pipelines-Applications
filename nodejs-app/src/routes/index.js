fce42c74bb55c11b2edeaba90b6c76f382025141
Juan Camilo Posada <juancapoar@gmail.com>
2020-05-23 06:52:58 -0500

    nodejs update

--- nodejs-app/src/routes/index.js
+++ nodejs-app/src/routes/index.js
@@ -1,6 +1,7 @@
 const {Router} = require('express');
 const router = Router();
 
-router.get('/',(req,res)=> res.json({message: 'Hola muchachos'}));
+router.get('/',(req,res)=> res.render(<div><h1>Aplicación 2 corriendo</h1></div>));
+//res.json({message: 'Hola muchachos'})
 
 module.exports = router;
