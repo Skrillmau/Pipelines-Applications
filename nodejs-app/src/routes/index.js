83058965a2b6e778d648783e462d44e4a5a9fa64
Julian Mateo Ballesteros Rojas <mate_balles@hotmail.com>
2020-05-23 06:48:12 -0500

    test

--- nodejs-app/src/routes/index.js
+++ nodejs-app/src/routes/index.js
@@ -1,6 +1,6 @@
 const {Router} = require('express');
 const router = Router();
 
-router.get('/',(req,res)=> res.json({message: 'Hola Choka'}));
+router.get('/',(req,res)=> res.json({message: 'Hola desde nodeapp y pipelines'}));
 
 module.exports = router;
