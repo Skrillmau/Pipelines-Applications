2c87dbeb9385027327e7538957395e05015d8d66
Juan Camilo Posada <juancapoar@gmail.com>
2020-05-23 08:00:19 -0500

    Update index.js

--- nodejs-app/src/routes/index.js
+++ nodejs-app/src/routes/index.js
@@ -1,6 +1,6 @@
 const {Router} = require('express');
 const router = Router();
 
-router.get('/',(req,res)=> res.send('<img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fes.wikipedia.org%2Fwiki%2FNode.js&psig=AOvVaw1gHx6aPLzNmYYcHRmNQ76q&ust=1590324784203000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCNDYtdCDyukCFQAAAAAdAAAAABAD" /><br /><h1>Aplicación de NodeJs corriendo modificada desde el pipeline y choko está bien pendejo</h1>'));
+router.get('/',(req,res)=> res.send('<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/1200px-Node.js_logo.svg.png" /><br /><h1>Aplicación de NodeJs corriendo modificada desde el pipeline</h1>'));
 
 module.exports = router;
