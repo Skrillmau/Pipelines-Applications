8e33cbb2455cef9edbc1c87b4c32bcb4e4679d91
Juan Camilo Posada <juancapoar@gmail.com>
2020-05-23 08:07:18 -0500

    Update index.js

--- nodejs-app/src/routes/index.js
+++ nodejs-app/src/routes/index.js
@@ -1,6 +1,6 @@
 const {Router} = require('express');
 const router = Router();
 
-router.get('/',(req,res)=> res.send('<h1 style="text-align:center;">Aplicación de NodeJs corriendo modificada desde el pipeline</h1><br /><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/1200px-Node.js_logo.svg.png" />'));
+router.get('/',(req,res)=> res.send('<h1 style="text-align:center;">Aplicación de NodeJs corriendo modificada desde el pipeline</h1><br /><img style="text-align:center;" src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/1200px-Node.js_logo.svg.png" />'));
 
 module.exports = router;
