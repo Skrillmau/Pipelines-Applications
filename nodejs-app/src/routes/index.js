75ba3904fb458b8ec61ca2cb011f5f465a103cba
Julian Mateo Ballesteros Rojas <mate_balles@hotmail.com>
2020-05-23 07:46:57 -0500

    imagen

--- nodejs-app/src/routes/index.js
+++ nodejs-app/src/routes/index.js
@@ -1,6 +1,6 @@
 const {Router} = require('express');
 const router = Router();
 
-router.get('/',(req,res)=> res.send('<img src="./node.png" /><br /><h1>Aplicación de NodeJs corriendo modificada desde el pipeline</h1>'));
+router.get('/',(req,res)=> res.send('<img src="../node.png" /><br /><h1>Aplicación de NodeJs corriendo modificada desde el pipeline</h1>'));
 
 module.exports = router;
