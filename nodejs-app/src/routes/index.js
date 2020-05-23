11ad2c44cb1997376a7d58974307250d6f402c93
Julian Mateo Ballesteros Rojas <mate_balles@hotmail.com>
2020-05-23 06:35:56 -0500

    cambio

--- nodejs-app/src/routes/index.js
+++ nodejs-app/src/routes/index.js
@@ -1,6 +1,6 @@
 const {Router} = require('express');
 const router = Router();
 
-router.get('/',(req,res)=> res.json({message: 'Hola muchachos'}));
+router.get('/',(req,res)=> res.json({message: 'Hola Choka'}));
 
 module.exports = router;
